51e4ac909d628c5324a95d0aaa5f4b2fd8a35b5e
LangaLee <langa4749@gmail.com>
2024-02-05 11:46:08 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ const {
 
 const cors = require("cors");
 
-app.use(cors());
-
 const apiRouter = require("./routers/api-router");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
