b83244e1516b78fc718322bf9828ea8de1bd4fe9
LangaLee <langa4749@gmail.com>
2024-03-13 14:31:23 +0000

    reseeded the production db

---
+++ db/data/development-data/likes.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { username: "jessjelly", article_id: 1, likes: 1 },
+  { username: "jessjelly", article_id: 2, likes: 1 },
+  { username: "jessjelly", article_id: 3, likes: -1 },
+  { username: "jessjelly", article_id: 4, likes: 1 },
+  { username: "tickle122", article_id: 2, likes: -1 },
+  { username: "tickle122", article_id: 3, likes: -1 },
+  { username: "tickle122", article_id: 4, likes: -1 },
+];
